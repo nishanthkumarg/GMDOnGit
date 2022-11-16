c7ad7bc8bc3fe820328e19e31f3ff4ca06d62839
nishanthkumarg <nishanthkumarg@google.com>
2022-11-16 14:33:43 -0800

    Code changes for GitCI for GMD

---
+++ GMDGitCIApp/app/src/androidTest/java/com/example/gmdgitciapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,56 @@
+package com.example.gmdgitciapp
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.gmdgitciapp", appContext.packageName)
+    }
+
+    //Passing tests
+    @Test
+    fun passingTest1(){
+        assert(false)
+    }
+
+    @Test
+    fun passingTest2(){
+        assert(false)
+    }
+
+    //Failing tests
+    @Test
+    fun failingTest1(){
+        assert(false)
+    }
+
+    @Test
+    fun failingTest2(){
+        assert(false)
+    }
+
+    @Test
+    fun failingTest3(){
+        assert(false)
+    }
+
+    @Test
+    fun failingTest4(){
+        assert(false)
+    }
+}
